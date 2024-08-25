f6294c127c5885d1c3c6af7adffbed2f0612c04e
redaAssemghor <assemghor.reda@gmail.com>
2024-08-25 22:58:17 +0100

    remove btn

--- src/features/pokemon/PokemonsList.tsx
+++ src/features/pokemon/PokemonsList.tsx
@@ -1,14 +1,12 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useGetPokemonsQuery } from "./pokemonApi";
 import PokemonCard from "./PokemonCard";
 import { useSelector } from "react-redux";
-import { Pokemon } from "./types";
 import { RootedState } from "../../store/store";
 
 const PokemonsList: React.FC = () => {
   const { data: pokemonList, isLoading, error } = useGetPokemonsQuery();
   const [showLikedPokemons, setShowLikedPokemons] = useState(false);
-  const [pokemonsArr, setPokemonsArr] = useState<Pokemon[]>([]);
 
   const deletedPokemons = useSelector(
     (state: RootedState) => state.pokemonActions.deletedPokemons
@@ -18,23 +16,10 @@ const PokemonsList: React.FC = () => {
     (state: RootedState) => state.pokemonActions.likedPokemons
   );
 
-  useEffect(() => {
-    console.log("deletedPokemons", deletedPokemons);
-    console.log("pokemonList", pokemonList);
-    console.log("liked", likedPokemons);
-  }, [deletedPokemons, pokemonList, likedPokemons]);
-
-  const handleFetch = () => {
-    if (pokemonList?.results) {
-      setPokemonsArr(pokemonList.results);
-    }
-    console.log("pokemonsArr", pokemonsArr);
-  };
-
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error fetching Pokémon</div>;
 
-  const filteredPokemons = pokemonsArr
+  const filteredPokemons = pokemonList?.results
     .filter((pokemon) => !deletedPokemons.includes(pokemon.name))
     .filter((pokemon) => {
       if (showLikedPokemons) {
@@ -45,23 +30,15 @@ const PokemonsList: React.FC = () => {
 
   return (
     <div className="m-4">
-      <div className="flex gap-1 justify-around">
-        <button
-          onClick={handleFetch}
-          className="mb-4 w-60 px-4 py-2 bg-gray-600 text-white rounded"
-        >
-          Fetch Pokémons
-        </button>
-        <button
-          onClick={() => setShowLikedPokemons(!showLikedPokemons)}
-          className="mb-4 w-60 px-4 py-2 bg-gray-600 text-white rounded"
-        >
-          {showLikedPokemons ? "Show All Pokémons" : "Show Liked Pokémons"}
-        </button>
-      </div>
+      <button
+        onClick={() => setShowLikedPokemons(!showLikedPokemons)}
+        className="mb-4 w-60 px-4 py-2 bg-gray-600 text-white rounded"
+      >
+        {showLikedPokemons ? "Show All Pokémons" : "Show Liked Pokémons"}
+      </button>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {filteredPokemons.map((pokemon) => (
+        {filteredPokemons?.map((pokemon) => (
           <PokemonCard key={pokemon.name} name={pokemon.name} />
         ))}
       </div>
